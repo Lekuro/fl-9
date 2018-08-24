9cd3942578e97e39bebf107c2994d540bddd74c8
Lekuro <lekurot@gmail.com>
2018-08-24 19:53:49 +0300

    initial commit hw9

---
+++ FE_9_9_homework_data-types/homework/index.js
@@ -0,0 +1,53 @@
+const findType = argument => typeof argument;
+
+const forEach = (array, callback) => {
+  for (let i = 0; i < array.length; i++) {
+    callback(array[i], i, array);
+  }
+};
+
+const map = (array, callback) => {
+  let arrayNew = [];
+  forEach(array, element => arrayNew.push(callback(element)));
+  return arrayNew;
+};
+
+const filter = (array, callback) => {
+  let arrayNew = [];
+  forEach(array, element => {
+    if (callback(element)) {
+      arrayNew.push(element);
+    }
+  });
+  return arrayNew;
+};
+
+const getAdultAppleLovers = array => {
+  let arrNeeded = filter(array, el => el.age > 18 && el.favoriteFruit === 'apple');
+  return map(arrNeeded, el => el.name);
+};
+
+const keys = obj => {
+  let arrKeys = [];
+  for (let key in obj) {
+    if (key) {
+      arrKeys.push(key);
+    }
+  }
+  return arrKeys;
+};
+
+const values = obj => {
+  let arrValues = [];
+  for (let key in obj) {
+    if (key) {
+      arrValues.push(obj[key]);
+    }
+  }
+  return arrValues;
+};
+
+const showFormattedDate = date => {
+  let arrMonth = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  return `It is ${date.getDate()} of ${arrMonth[date.getMonth()]}, ${date.getFullYear()}`;
+};
